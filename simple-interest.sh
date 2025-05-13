7b734e33cf01053f162e317418ee70f56f522c4a
Haripriya <110587904+haripriya624@users.noreply.github.com>
2025-05-13 18:14:23 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-echo "Enter Principal:"
-read p
-echo "Enter Rate of Interest:"
-read r
-echo "Enter Time (in years):"
-read t
-
-si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
-echo "Simple Interest is: $si"
+echo "Enter the principal:"
+   read p
+   echo "Enter rate of interest per year:"
+   read r
+   echo "Enter time period in years:"
+   read t
+   s=`expr $p \* $t \* $r / 100`
+   echo "The simple interest is: "
+   echo $s
